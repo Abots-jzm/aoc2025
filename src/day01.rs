d0e0eca83f1ef2e91c673711dd9d172390141dd1
Tobi Abolarin <abolarinoluwatobistephen@gmail.com>
2025-12-04 09:41:54 -0600

    day 1 part 1

---
+++ src/day01.rs
@@ -0,0 +1,74 @@
+use crate::read_input;
+
+pub fn solve() {
+    let input = read_input(1);
+
+    println!("  Part 1: {}", part1(&input));
+    println!("  Part 2: {}", part2(&input));
+}
+
+fn part1(input: &str) -> isize {
+    let mut position = 50;
+    // let mut counter = 0;
+    input
+        .lines()
+        .map(|line| line.parse::<Turn>().unwrap())
+        .fold(0, |acc, turn| {
+            position = match turn {
+                Turn::Left(n) => (position - n).rem_euclid(100),
+                Turn::Right(n) => (position + n).rem_euclid(100),
+            };
+            if position == 0 { acc + 1 } else { acc }
+        })
+}
+
+fn part2(input: &str) -> i64 {
+    // TODO: Implement part 2
+    0
+}
+
+enum Turn {
+    Left(i32),
+    Right(i32),
+}
+
+impl std::str::FromStr for Turn {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (dir, num) = s.split_at(1);
+        let value: i32 = num.parse().expect("should be a valid number");
+        match dir {
+            "L" => Ok(Turn::Left(value)),
+            "R" => Ok(Turn::Right(value)),
+            _ => Err(format!("Invalid turn direction: {}", dir)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT), 3);
+    }
+
+    // #[test]
+    // fn test_part2() {
+    //     assert_eq!(part2(EXAMPLE), 0);
+    // }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod day01;
+
+fn main() {
+    println!("🎄 Advent of Code 2025 🎄\n");
+
+    println!("Day 1:");
+    day01::solve();
+}
+
+// Helper function to read input file for a given day
+pub fn read_input(day: u8) -> String {
+    let path = format!("inputs/day{:02}.txt", day);
+    std::fs::read_to_string(&path).unwrap_or_else(|_| panic!("Could not read input file: {}", path))
+}
+
+// Helper to read input as lines
+pub fn read_lines(day: u8) -> Vec<String> {
+    read_input(day).lines().map(|s| s.to_string()).collect()
+}
+
+// Helper to parse input as numbers
+pub fn read_numbers<T: std::str::FromStr>(day: u8) -> Vec<T>
+where
+    T::Err: std::fmt::Debug,
+{
+    read_input(day)
+        .lines()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
