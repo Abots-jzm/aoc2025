9322a94697436b683528b43dac9ca3e658e4b697
Tobi Abolarin <abolarinoluwatobistephen@gmail.com>
2025-12-13 17:32:07 -0600

    day 2 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -53,7 +53,60 @@ fn part1(input: &str) -> isize {
 }
 
 fn part2(input: &str) -> isize {
-    0
+    input
+        .split(',')
+        .map(|range| {
+            range
+                .split_once('-')
+                .expect("there should be a '-' in the range")
+        })
+        .fold(0, |acc, (start, end)| {
+            let start_num = start.parse::<isize>().unwrap();
+            let end_num = end.parse::<isize>().unwrap();
+            let mut seen = std::collections::HashSet::new();
+
+            let mut total = 0;
+            let mut chunks = 2;
+
+            while chunks <= end.len() {
+                let chunk_len = start.len() / chunks;
+                let chunk = &start[..chunk_len];
+                let mut chunk_num = chunk.parse::<isize>().unwrap_or(0);
+                if start.len() % chunks != 0 {
+                    if chunk_num == 0 {
+                        chunk_num = 1
+                    } else {
+                        let digits = (chunk_num as f64).log10().floor() as u32 + 1;
+                        chunk_num = 10_u64.pow(digits) as isize;
+                    }
+                };
+
+                loop {
+                    let cur_num = chunk_num
+                        .to_string()
+                        .repeat(chunks)
+                        .parse::<isize>()
+                        .unwrap();
+                    chunk_num += 1;
+
+                    if cur_num < start_num {
+                        continue;
+                    }
+
+                    if cur_num > end_num {
+                        break;
+                    }
+
+                    if !seen.contains(&cur_num) {
+                        total += cur_num;
+                        seen.insert(cur_num);
+                    }
+                }
+
+                chunks += 1;
+            }
+            acc + total
+        })
 }
 
 #[cfg(test)]
@@ -67,8 +120,8 @@ mod tests {
         assert_eq!(part1(INPUT), 1227775554);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(INPUT), 6);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 4174379265);
+    }
 }
